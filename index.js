54d4bd79617b68643001bd5e435fac1d0d68c5bd
Chad <a089831918@gmail.com>
2024-08-04 23:04:37 +0800

    boilerplate-project-headerparser

--- index.js
+++ index.js
@@ -21,9 +21,12 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+  res.json({ greeting: 'hello API' });  
 });
 
+app.get('/api/whoami',function(req,res){
+  res.json({ ipaddress: "127.0.0.1" , "language":"zh_TW" , "software":"LuTwoTwo" })
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
